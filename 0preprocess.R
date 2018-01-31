485e97d5bafda72903dbfc608f61a85e16d91888
tpoorten <tom.poorten@gmail.com>
2018-01-30 16:40:09 -0800

    add error messages for read_leaf()

--- 0preprocess.R
+++ 0preprocess.R
@@ -1,10 +1,10 @@
 library(here)
-source(paste0(here(),"/src/commands.R"))
+source(paste0(here(),"/src/functions.R"))
 
 ## Read in data
-dat = read_leaf(snp_data_file  = "input_data/SNP_data.txt", 
-               sample_qc_file = "input_data/Sample_QC.txt",
-               ps_qc_file     = "input_data/Ps.performance.txt",
+dat = read_leaf(snp_data_file   = "input_data/SNP_data.txt", 
+               sample_qc_file   = "input_data/Sample_QC.txt",
+               ps_qc_file       = "input_data/Ps.performance.txt",
                sample_meta_file = "input_data/sample_master_list.csv"
               )
 
