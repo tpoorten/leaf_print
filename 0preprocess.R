d2e7e031318e507b483b7efb6676b291f9bd6086
tpoorten <tom.poorten@gmail.com>
2018-04-09 12:00:08 -0700

    edits to leaf_read, leaf_filter in functions; add GDS format conversion to script

--- 0preprocess.R
+++ 0preprocess.R
@@ -4,22 +4,51 @@ source(paste0(here(),"/src/functions.R"))
 ## Read in data
 dat = leaf_read(snp_data_file   = "input_data/SNP_data.txt", 
                sample_qc_file   = "input_data/Sample_QC.txt",
+               sample_meta_file = "input_data/sample_master_list.csv",
                ps_qc_file       = "input_data/Ps.performance.txt",
-               sample_meta_file = "input_data/sample_master_list.csv"
+               snp_meta_file    = "input_data/snp_metadata_v2.txt"
               )
 
-head(dat$snp_data)
-table(dat$ps_qc$H.W.p.Value)
+head(dat$snp_data[,1:3])
+head(dat$ps_qc)
 head(dat$sample_data)
 
 # //
 
+## Filter SNPs
 dat1 = leaf_filter(leaf_data = dat, filter_col1 = "ConversionType", condition1 = "PolyHighResolution", filter_col2 = "BestProbeset", condition2 = 1)
 
-head(dat1$snp_data)
-nrow(dat1$ps_qc)
+dim(dat$snp_data)
+dim(dat1$snp_data)
+dim(dat$ps_qc)
+dim(dat1$ps_qc)
 head(dat1$sample_data)
 
 ## 
-dat2 = recalculate_sample_metrics(dataList = dat)
+dat2 = leaf_recalculate_sample_metrics(dataList = dat1)
 
+head(dat1$sample_data)
+head(dat2$sample_data)
+# //
+
+
+## Create a gds file
+library(SNPRelate)
+# table(as.matrix(snp))
+# length(unique(colnames(snp))) == ncol(snp)
+table((dat1$ps_qc$istraw90_Chromsome))
+table(is.na(dat1$ps_qc$istraw90_Chromsome))
+table((dat1$ps_qc$bwaFv4.0_RNAME))
+table(is.na(dat1$ps_qc$bwaFv4.0_RNAME))
+#
+dir.create("RDataset", showWarnings = F)
+snpgdsCreateGeno("RDataset/snp.gds", genmat = as.matrix(dat1$snp_data),
+                 sample.id = colnames(dat1$snp_data), snp.id = rownames(dat1$snp_data),
+                 snp.chromosome = dat1$ps_qc$istraw90_Chromsome,
+                 snp.position = dat1$ps_qc$istraw90_Position,
+                 snp.allele = paste(dat1$ps_qc[,c("Allele.Ref")], dat1$ps_qc[,c("Allele.Alt")], sep = "/"), snpfirstdim=TRUE)
+# make plink
+# genofile <- snpgdsOpen("RDataset/snp.gds")
+# geno.sample.ids = read.gdsn(index.gdsn(genofile, "sample.id"))
+# snpgdsGDS2PED(genofile, "RDataset/snp", snp.id = dat1$ps_qc$probeset_id)
+# snpgdsClose(genofile)
--- src/functions.R
+++ src/functions.R
@@ -3,44 +3,79 @@
 
 ################################################
 #The purpose of this function is to read and compile all sample, SNP, and metadata.
-leaf_read = function(snp_data_file=NULL, ps_qc_file = NULL, sample_qc_file=NULL, sample_meta_file=NULL){
+leaf_read = function(snp_data_file=NULL, ps_qc_file = NULL, sample_qc_file=NULL, sample_meta_file=NULL, snp_meta_file = NULL){
+  print("Reading in SNP data ...")
+  # SNP DATA
   if(file.exists(snp_data_file)){
     snp_data = read.table(snp_data_file, sep="\t", header=T, stringsAsFactors = F, check.names = F)
+    colnames(snp_data) = gsub("_call_code","",colnames(snp_data))
     rownames(snp_data) = snp_data$probeset_id
     snp_data = snp_data[,which(colnames(snp_data) != "probeset_id")]
   } else {stop("Error: invalid filename for 'snp_data_file'")}
   
+  print("Reading in metadata files ...")
+  # SAMPLE_QC - from Axiom Analysis Suite output
   if(file.exists(sample_qc_file)){
     sample_qc = read.table(sample_qc_file, sep="\t", header=T, stringsAsFactors = F)
     sample_qc = sample_qc[,1:7]
+    rownames(sample_qc) = sample_qc$Sample.Filename
+    # keep on CELs in 'snp_data'
+    sample_qc = sample_qc[match(colnames(snp_data), rownames(sample_qc)),]
   } else {stop("Error: invalid filename for 'sample_qc_file'")}
   
-  if(file.exists(ps_qc_file)){
-    ps_qc = read.table(ps_qc_file, sep="\t", header=T, stringsAsFactors = F)
-    rownames(ps_qc) = ps_qc$probeset_id
-    if(identical(sort(rownames(snp_data)),sort(ps_qc$probeset_id))){
-      ps_qc = ps_qc[match(rownames(snp_data),ps_qc$probeset_id),]
-    } else {stop("Error: probeset IDs do not match between 'snp_data_file' and 'ps_qc_file'")}
-  } else {stop("Error: invalid filename for 'ps_qc_file'")}
-    
+  # SAMPLE METADATA - any extra sample metadata to add to sample_qc
   sample_metadata = NULL
+  # sample_all_data = NULL
   if(!is.null(sample_meta_file)){
     if(file.exists(sample_meta_file)){
       sample_metadata = read.csv(sample_meta_file, stringsAsFactors = F)
       sample_metadata = sample_metadata[match(colnames(snp_data),sample_metadata[,1]),]
+      rownames(sample_metadata) = sample_metadata$Best.Array
+      sample_qc = merge(sample_qc, sample_metadata, by = 1) # merge by column 1
+      rownames(sample_qc) = sample_qc$Sample.Filename
     } else {stop("Error: invalid filename for sample_meta_file. This file can be omitted.")}
   }
   
-  sample_all_data = NULL
-  if(!is.null(sample_meta_file)){
-    if(file.exists(sample_meta_file)){
-      sample_all_data = merge(sample_qc, sample_metadata, by = 1)
+  # PROBESET QC - from Axiom Analysis Suite output
+  if(file.exists(ps_qc_file)){
+    ps_qc = read.table(ps_qc_file, sep="\t", header=T, stringsAsFactors = F)
+    rownames(ps_qc) = ps_qc$probeset_id
+    if(identical(sort(rownames(snp_data)),sort(rownames(ps_qc)))){
+      ps_qc = ps_qc[match(rownames(snp_data),rownames(ps_qc)),]
+    } else {stop("Error: probeset IDs do not match between 'snp_data_file' and 'ps_qc_file'")}
+  } else {stop("Error: invalid filename for 'ps_qc_file'")}
+  
+  # PROBESET METADATA - any extra snp metadata to add to sample_qc
+  if(!is.null(snp_meta_file)){
+    if(file.exists(snp_meta_file)){
+      snp_metadata = read.table(snp_meta_file, header=T, sep="\t", stringsAsFactors = F)
+      # rownames(snp_metadata) = snp_metadata$probeset_id
+      ps_qc = merge(ps_qc, snp_metadata, by = 1) # merge by column 1
+      rownames(ps_qc) = ps_qc$probeset_id
     } else {stop("Error: invalid filename for sample_meta_file. This file can be omitted.")}
   }
+  
+  print(paste0("snp_data   (data.frame)  : ",nrow(snp_data)," SNPs (rows)  x  ", ncol(snp_data), " CELs (columns)"))
+  print(paste0("ps_qc      (data.frame)  : ",nrow(ps_qc)," SNPs (rows)  x  ", ncol(ps_qc), " info columns"))
+  print(paste0("sample_qc  (data.frame)  : ",nrow(sample_qc)," samples (rows)  x  ", ncol(sample_qc), " info columns"))
+
+  if(identical(rownames(ps_qc), rownames(snp_data))){
+    print("    Rows in 'ps_qc' matches up with rows in 'snp_data'")
+  } else {
+    print("    Rows in 'ps_qc' DOES NOT match up with rows in 'snp_data'")
+  }
+  
+  if(identical(rownames(sample_qc), colnames(snp_data))){
+    print("    Rows in 'sample_qc' matches up with columns in 'snp_data'")
+  } else {
+    print("    Rows in 'sample_qc' DOES NOT match up with columns in 'snp_data'")
+  }
+  print("DONE")
   
   leaf = list(snp_data        = snp_data, 
               ps_qc           = ps_qc, 
-              sample_data     = sample_all_data)
+              sample_data     = sample_qc)
+  
   return(leaf)
 }
 
@@ -53,15 +88,27 @@ leaf_filter = function(leaf_data = NULL, filter_col1 = "ConversionType", conditi
     
     if(!is.null(filter_col1)){
     leaf_data$ps_qc = leaf_data$ps_qc[which(leaf_data$ps_qc[,filter_col1] == condition1),]
-    leaf_data$snp_data = leaf_data$snp_data[rownames(leaf_data$ps_qc),]
+    leaf_data$snp_data = leaf_data$snp_data[match(rownames(leaf_data$ps_qc), rownames(leaf_data$snp_data)),]
     } else {stop("Error: Require at least one column and one condition")}
     
     if(!is.null(filter_col2)){
       leaf_data$ps_qc = leaf_data$ps_qc[which(leaf_data$ps_qc[,filter_col2] == condition2),]
-      leaf_data$snp_data = leaf_data$snp_data[rownames(leaf_data$ps_qc),]
+      leaf_data$snp_data = leaf_data$snp_data[match(rownames(leaf_data$ps_qc), rownames(leaf_data$snp_data)),]
     } else {}
     
   } else {stop("Error: Object Not Found")}
+  
+  if(identical(rownames(leaf_data$ps_qc), rownames(leaf_data$snp_data))){
+    print("    Rows in 'ps_qc' matches up with rows in 'snp_data'")
+  } else {
+    print("    Rows in 'ps_qc' DOES NOT match up with rows in 'snp_data'")
+  }
+  
+  if(identical(rownames(leaf_data$sample_data), colnames(leaf_data$snp_data))){
+    print("    Rows in 'sample_qc' matches up with columns in 'snp_data'")
+  } else {
+    print("    Rows in 'sample_qc' DOES NOT match up with columns in 'snp_data'")
+  }
   
   leaf = list(snp_data        = leaf_data$snp_data, 
               ps_qc           = leaf_data$ps_qc, 
